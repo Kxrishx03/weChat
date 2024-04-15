a2906562b039cfe69ce45bdfd39364bdbd4d6f1d
Kxrishx03 <2001karishmamandal@gmail.com>
2024-04-15 19:01:35 +0530

    "Routing -init"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import { Login } from './components/Login'
 import { MainContainer } from './components/MainContainer'
+import {Routes,Route} from 'react-router-dom';
+import { Welcome } from './components/Welcome';
+import { ChatArea } from './components/ChatArea';
 function App() {
 
 
   return (
     <>
       <div className='app_container'>
-           {/*<Login/>*/}
-           <MainContainer />
+          <Routes >
+            <Route  path="/" element={<Login/>}/>
+
+            <Route  path='app' element={<MainContainer />}>
+            <Route path='welcome' element={<Welcome/>}/>
+            <Route path='chat' element={<ChatArea/>} />
+            </Route>
+          </Routes>
+           
+           
       </div>
     </>
   )
